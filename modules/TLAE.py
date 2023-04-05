fa0ac1c9405cb4ada6ca47e4db5c0bc0ea147723
an-seunghwan <dpeltms79@gmail.com>
2023-04-05 19:57:16 +0900

    update TVAE modules

--- modules/TLAE.py
+++ modules/TLAE.py
@@ -17,130 +17,67 @@ posterior = namedtuple(
     'posterior', 
     ['z', 'mean', 'logvar'])
 #%%
-class GLD(nn.Module):
+class TLAE(nn.Module):
     def __init__(self, config, device):
-        super(GLD, self).__init__()
+        super(TLAE, self).__init__()
         self.config = config
-        self.M = config["M"]
         self.device = device
         
-        """Generative model"""
-        self.fc_C = nn.Linear(config["p"], config["d_model"])
-        self.add_posit_C = layers.AddPosition2(config["d_model"], config["timesteps"], device)
-        self.prior = layers.PriorModule(self.config, device) 
+        """encoder"""
+        self.encoder = nn.Sequential(
+            nn.Linear(config["p"], config["d_latent"]),
+            nn.ELU(),
+            nn.Linear(config["d_latent"], config["d_latent"]),
+            nn.ELU(),
+            nn.Linear(config["d_latent"], config["d_latent"])).to(device)
+        
+        """temporal structure"""
+        self.lstm = nn.LSTM(config["d_latent"], config["d_latent"], config["num_layer"], batch_first=True).to(self.device)
 
-        """Inference model"""
-        self.fc_T = nn.Linear(config["p"], config["d_model"])
-        self.add_posit_T = layers.AddPosition2(config["d_model"], config["timesteps"] + config["future"], device)
-        self.posterior = layers.PosteriorModule(self.config, self.prior, device) 
+        """decoder"""
+        self.decoder = nn.Sequential(
+            nn.Linear(config["d_latent"], config["d_latent"]),
+            nn.ELU(),
+            nn.Linear(config["d_latent"], config["d_latent"]),
+            nn.ELU(),
+            nn.Linear(config["d_latent"], config["p"])).to(device)
+    
+    def get_prior(self, context_batch, deterministic=False):
+        z = self.encoder(context_batch)
+    
+        out, (hn, cn) = self.lstm(z)
         
-        self.spline = nn.ModuleList(
-            [nn.Linear(config["d_latent"], 4 * config["p"])
-             for _ in range(config["timesteps"] + config["future"])])
-        # self.spline = nn.ModuleList(
-        #     [nn.Sequential(
-        #         nn.Linear(config["d_latent"], 16),
-        #         nn.ELU(),
-        #         nn.Linear(16, 4 * config["p"])) 
-        #     for _ in range(config["timesteps"] + config["future"])])
-    
-    def quantile_parameter(self, h):
-        h = torch.split(h, 4, dim=1)
-        theta1 = [h_[:, [0]].tanh() for h_ in h]
-        theta2 = [(h_[:, [1]]).exp() for h_ in h]
-        # finite support
-        theta3 = [(h_[:, [2]]).exp() for h_ in h]
-        theta4 = [(h_[:, [3]]).exp() for h_ in h]
-        return theta1, theta2, theta3, theta4
-    
-    def get_prior(self, context_batch):
-        h_C = self.add_posit_C(self.fc_C(context_batch))
-        prior_w, prior_z, prior_mean, prior_logvar = self.prior(h_C)
-        return prior_w, prior_z, prior_mean, prior_logvar
-    
-    def get_spline(self, z):
-        spline_feature = list(map(lambda x, d: d(x.squeeze()), z, self.spline))
-        params = list(map(lambda x: self.quantile_parameter(x), spline_feature))
-        return params
-    
-    """Generalized Lambda distribution"""
-    def quantile_function(self, tau, theta1, theta2, theta3, theta4):
-        Q = (tau ** theta3 - 1) / theta3
-        Q -= ((1 - tau) ** theta4 - 1) / theta4
-        return theta1 + 1 / theta2 * Q
-    
-    def forward(self, context_batch, target_batch):
-        h_C = self.add_posit_C(self.fc_C(context_batch))
-        h_T = self.add_posit_T(self.fc_T(target_batch))
+        future_mu = [out[:, [-1], :]]
+        future_z = [future_mu[-1] + torch.randn(future_mu[-1].shape)]
         
-        prior_z_list = []
-        prior_mean_list = []
-        prior_logvar_list = []
-        
-        posterior_z_list = []
-        posterior_mean_list = []
-        posterior_logvar_list = []
-        
-        for i in range(self.config["num_layer"]):
-            if i == 0:
-                prior_w, prior_z, prior_mean, prior_logvar = self.prior(h_C)
-                posterior_w, posterior_z, posterior_mean, posterior_logvar = self.posterior(h_C, h_T)
+        for _ in range(self.config["future"]-1):
+            if deterministic:
+                out, (hn, cn) = self.lstm(future_mu[-1], (hn, cn))
             else:
-                prior_w, prior_z, prior_mean, prior_logvar = self.prior(h_C, torch.cat(prior_w, dim=1))
-                posterior_w, posterior_z, posterior_mean, posterior_logvar = self.posterior(h_C, h_T, torch.cat(posterior_w, dim=1))
-
-            prior_z_list.append(prior_z)
-            prior_mean_list.append(prior_mean)
-            prior_logvar_list.append(prior_logvar)
-            
-            posterior_z_list.append(posterior_z)
-            posterior_mean_list.append(posterior_mean)
-            posterior_logvar_list.append(posterior_logvar)
-            
-        params = self.get_spline(posterior_z)
+                out, (hn, cn) = self.lstm(future_z[-1], (hn, cn))
+            future_mu.append(out)
+            future_z.append(out + torch.randn(out.shape))
         
-        return (prior(prior_z_list, prior_mean_list, prior_logvar_list), 
-                posterior(posterior_z_list, posterior_mean_list, posterior_logvar_list),
-                params)
+        future_mu = torch.cat(future_mu, dim=1)
+        future_z = torch.cat(future_z, dim=1)
+        z = torch.cat([z, future_z], dim=1)
+        return z, future_mu, future_z
+    
+    def forward(self, context_batch, deterministic=False):
+        z, future_mu, future_z = self.get_prior(context_batch, deterministic=deterministic)
+        xhat = self.decoder(z)
+        return xhat, future_mu, future_z
     
     def est_quantile(self, test_context, alphas, MC, disable=False):
-        est_quantiles = []
-        for a in alphas:
-            Qs = []
-            for _ in tqdm.tqdm(range(MC), desc=f"Quantile estimation...(alpha={a})", disable=disable):
-                with torch.no_grad():
-                    _, prior_z, _, _ = self.get_prior(test_context.to(self.device))
-                    params = self.get_spline(prior_z)
-                
-                theta1 = torch.cat(params[-1][0], dim=0)
-                theta2 = torch.cat(params[-1][1], dim=0)
-                theta3 = torch.cat(params[-1][2], dim=0)
-                theta4 = torch.cat(params[-1][3], dim=0)
-                
-                alpha = (torch.ones(theta1.shape) * a).to(self.device)
-                
-                Qs.append(self.quantile_function(
-                    alpha, theta1, theta2, theta3, theta4).reshape(test_context.size(0), self.config["p"])[:, None, :])
-            Qs = torch.cat(Qs, dim=1)
-            est_quantiles.append(Qs.mean(dim=1).cpu())
-        return est_quantiles, Qs
-    
-    def sampling(self, test_context, MC, disable=False):
         samples = []
-        for _ in tqdm.tqdm(range(MC), desc=f"Data sampling...", disable=disable):
+        for _ in tqdm.tqdm(range(MC), desc=f"Sampling...", disable=disable):
             with torch.no_grad():
-                _, prior_z, _, _ = self.get_prior(test_context.to(self.device))
-                params = self.get_spline(prior_z)
-            
-            theta1 = torch.cat(params[-1][0], dim=0)
-            theta2 = torch.cat(params[-1][1], dim=0)
-            theta3 = torch.cat(params[-1][2], dim=0)
-            theta4 = torch.cat(params[-1][3], dim=0)
-            
-            alpha = torch.rand(theta1.shape).to(self.device)
-            
-            samples.append(self.quantile_function(
-                alpha, theta1, theta2, theta3, theta4).reshape(test_context.size(0), self.config["p"])[:, None, :])
+                xhat, _, _ = self.forward(test_context, deterministic=True)
+                samples.append(xhat[:, self.config["timesteps"]:, :])
         samples = torch.cat(samples, dim=1)
-        return samples.cpu()
+        
+        est_quantiles = []
+        for i, a in enumerate(alphas):
+            est_quantiles.append(samples.quantile(q=a, dim=1))
+        return est_quantiles, samples
 #%%
--- modules/TLAE_train.py
+++ modules/TLAE_train.py
@@ -5,17 +5,11 @@ import torch
 # import wandb
 #%%
 def train_function(context, target, model, iterations, config, optimizer, device):
-    tau = torch.linspace(0.01, 0.99, config["K"]).unsqueeze(0).to(device)
-    # tau = torch.cat([
-    #     torch.linspace(0.01, 0.09, 9).unsqueeze(0),
-    #     torch.linspace(0.1, 0.9, config["K"]).unsqueeze(0)    
-    # ], dim=1)
     
     logs = {
         'loss': 0,
-        'quantile': 0,
+        'recon': 0,
         'KL': 0,
-        'active': 0,
     }
     for i in range(iterations):
         idx = np.random.choice(
@@ -25,49 +19,18 @@ def train_function(context, target, model, iterations, config, optimizer, device
         
         optimizer.zero_grad()
         
-        prior, posterior, params = model(context_batch, target_batch)
+        xhat, future_mu, future_z = model(context_batch)
         
-        j = 0 # coin
-        quantile_sum = 0
-        for j in range(target_batch.size(-1)):
-            target_batch_ = target_batch[..., j].reshape(-1, 1)
-            
-            theta1 = torch.cat([params[i][0][j][:, None, :] for i in range(len(params))], dim=1) # i = time
-            theta2 = torch.cat([params[i][1][j][:, None, :] for i in range(len(params))], dim=1) # i = time
-            theta3 = torch.cat([params[i][2][j][:, None, :] for i in range(len(params))], dim=1) # i = time
-            theta4 = torch.cat([params[i][3][j][:, None, :] for i in range(len(params))], dim=1) # i = time
-            
-            theta1 = theta1.reshape(-1, theta1.size(-1))
-            theta2 = theta2.reshape(-1, theta2.size(-1))
-            theta3 = theta3.reshape(-1, theta3.size(-1))
-            theta4 = theta4.reshape(-1, theta4.size(-1))
-            
-            Q = model.quantile_function(tau, theta1, theta2, theta3, theta4)
-            residual = target_batch_ - Q
-            quantile = (residual * (tau - (residual < 0).to(torch.float32))).sum()
-            quantile /= (config["K"] * context_batch.size(0))
-            quantile_sum += quantile
-        logs["quantile"] = logs.get("quantile") + quantile_sum
+        """Reconstruction"""
+        recon = (target_batch - xhat).abs().sum(dim=-1).mean()
+        logs["recon"] = logs.get("recon") + recon
         
         """KL-divergence"""
-        prior_mean = torch.cat([torch.cat(prior.mean[i], dim=0) for i in range(len(prior.mean))], dim=0)
-        prior_logvar = torch.cat([torch.cat(prior.logvar[i], dim=0) for i in range(len(prior.logvar))], dim=0)
-        posterior_mean = torch.cat([torch.cat(posterior.mean[i], dim=0) for i in range(len(posterior.mean))], dim=0)
-        posterior_logvar = torch.cat([torch.cat(posterior.logvar[i], dim=0) for i in range(len(posterior.logvar))], dim=0)
-        
-        KL = ((posterior_mean - prior_mean).pow(2) / prior_logvar.exp()).sum(dim=1)
-        KL += (prior_logvar - posterior_logvar).sum(dim=1)
-        KL += (posterior_logvar.exp() / prior_logvar.exp()).sum(dim=1)
-        KL -= config["d_latent"]
-        KL *= 0.5
-        KL = KL.sum() / context_batch.size(0)
+        KL = (future_mu - future_z).pow(2).sum(dim=-1).mean() * 0.5
         logs["KL"] = logs.get("KL") + KL
         
-        loss = quantile_sum + config["beta"] * KL
+        loss = recon + config["beta"] * KL
         logs["loss"] = logs.get("loss") + loss
-        
-        active = (posterior_logvar.exp().mean(dim=0) < 0.1).to(torch.float32).mean()
-        logs["active"] = logs.get("active") + active
         
         loss.backward()
         optimizer.step()
