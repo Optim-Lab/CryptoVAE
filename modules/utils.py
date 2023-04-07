8c06a0ddabdc17186fe65c829398a932e4b7888a
an-seunghwan <dpeltms79@gmail.com>
2023-04-07 18:26:14 +0900

    utils: add quantile visualization function

--- modules/utils.py
+++ modules/utils.py
@@ -3,6 +3,11 @@ import numpy as np
 import random
 import torch
 import yaml
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ing_theme_matplotlib import mpl_style # pip install ing_theme_matplotlib 
+import matplotlib as mpl
+import tqdm
 #%%
 """for reproducibility"""
 def set_random_seed(seed):
@@ -21,4 +26,45 @@ def load_config(config):
         if key in args.keys():
             config[key] = args[key]
     return config
+#%%
+def visualize_quantile(target, test_target, full_est_quantiles, est_quantiles, colnames, config, show=False, dark=False):
+    cols = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    
+    mpl.rcParams["figure.dpi"] = 200
+    mpl_style(dark=dark)
+    SMALL_SIZE = 10
+    BIGGER_SIZE = 18
+
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+    
+    figs = []
+    for j in tqdm.tqdm(range(len(colnames)), desc=f"Visualize Quantiles...", disable=show):
+        fig = plt.figure(figsize=(12, 7))   
+        conf = plt.fill_between(
+            np.arange(target.shape[0] - test_target.shape[0], target.shape[0]), 
+            est_quantiles[0][:, j].numpy(), 
+            est_quantiles[2][:, j].numpy(), 
+            color='blue', alpha=0.3, label=r'80% interval')
+        plt.plot(
+            target.numpy()[:, j],
+            label=colnames[j], color='black', linestyle='--', linewidth=2)
+        plt.plot(
+            full_est_quantiles[1][:, j].numpy(),
+            label='Median', color=cols[7], linewidth=2)
+        plt.xlabel('Date', fontsize=18)
+        plt.ylabel('Price', fontsize=18)
+        plt.legend(loc = 'upper left')    
+        plt.savefig(f'./assets/{config["model"]}/plots(future={config["future"]})/{colnames[j]}.png')
+        if show:
+            plt.show()
+        # plt.show()
+        plt.close()
+        figs.append(fig)
+    return figs
 #%%
